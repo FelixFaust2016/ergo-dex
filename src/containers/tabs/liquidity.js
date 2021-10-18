05fb882ba0313cc24a9304925b44dfa1b5616e19
FFaust <v.iheukwumere@genesystechhub.com>
2021-10-18 11:29:48 +0100

    updates

--- src/containers/tabs/liquidity.js
+++ src/containers/tabs/liquidity.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { colors } from "../../colors";
 
